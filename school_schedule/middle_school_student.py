c91e8408b2697bfe6cbcf2b09ab157af16f251f0
AstryBrana <astry.brana@gmail.com>
2024-10-02 14:26:49 -0400

    Attempt: MiddleSchoolStudent

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,17 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, get_transportation=False):
+        super().__init__(name, grade, classes)
+        self.get_transportation = get_transportation if get_transportation else []
+    
+    def display_transportation_message(self):
+        has_message = "has" if self.get_transportation else "doesn't have"
+        return f"{self.name} {has_message} transportation"
+    
+    def summary(self):
+        student_summary = super().summary()
+        transportation_message = self.display_transportation_message()
+        return "\n".join((student_summary, transportation_message))
+
+n = MiddleSchoolStudent("John", "6th", ["Geography"], get_transportation=False)
